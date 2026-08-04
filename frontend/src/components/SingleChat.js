5cd558c3621ab6afdc401cbae56a813cc1e48b7d
Simran <sw219335@gmail.com>
2026-08-05 02:10:13 +0530

    Fix Render startup error

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -88,7 +88,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
           "/api/message",
           {
             content: newMessage,
-            chatId: selectedChat,
+            chatId: selectedChat._id,
           },
           config
         );
